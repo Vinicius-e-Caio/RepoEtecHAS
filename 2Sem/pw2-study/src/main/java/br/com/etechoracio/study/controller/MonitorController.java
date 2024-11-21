117bf8ffb0c974fda84e56056413626c0d095c3e
viniciusdevjesus <viniciusjesusfilho08@gmail.com>
2024-11-21 00:30:58 -0300

    feat: verification for monitor registration

--- 2Sem/pw2-study/src/main/java/br/com/etechoracio/study/controller/MonitorController.java
+++ 2Sem/pw2-study/src/main/java/br/com/etechoracio/study/controller/MonitorController.java
@@ -2,15 +2,24 @@ package br.com.etechoracio.study.controller;
 
 import br.com.etechoracio.study.entity.Disciplina;
 import br.com.etechoracio.study.entity.Monitor;
+import br.com.etechoracio.study.service.DisciplinaService;
+import br.com.etechoracio.study.service.MonitorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/monitores")
 public class MonitorController {
+    @Autowired
+    private MonitorService service;
     @PostMapping("/cadastrar")
     public ResponseEntity<Monitor> cadastrarMonitor(@RequestBody Monitor monitor){
-        var novaMonitor = service.cadastrarDisciplina(disciplina);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novaDisciplina);
+        var newMonitor = service.cadastrarMonitor(monitor);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMonitor);
     }
 }
